fe699a4e8a08ad2b586a8e6cc324266a0a2e55ab
gohilamariappan <41056032+gohilamariappan@users.noreply.github.com>
2019-04-05 14:27:49 +0530

    hi

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key_id"
 {
-default = "AKIAXXSDVOLSGXPWPKBB"
+default = ""
 }
 
 variable "aws_secret_access_key"
 {
 
-default = "NbVm8YYua5+U80uoueoh64kq/gJZhGH5C7BDAtzo"
+default = ""
 }
 

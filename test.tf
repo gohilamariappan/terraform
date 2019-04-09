1f18de408d2b1ae8819fa5d1cae60dc91ae95177
gohilamariappan <41056032+gohilamariappan@users.noreply.github.com>
2019-04-09 15:42:24 +0530

    changed

--- test.tf
+++ test.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "${var.aws_access_key_id}"
   secret_key = "${var.aws_secret_access_key}"
-  region     = "ap-south-1"
+  region     = "us-east-2"
 }
 resource "aws_instance" "web" {
 
